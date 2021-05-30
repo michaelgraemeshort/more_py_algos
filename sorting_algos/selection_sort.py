fd3a7425a7c9191b95da0696554ad61ad68276d8
michaelgraemeshort <michaelgraemeshort@gmail.com>
2021-05-30 22:28:16 +0100

    sorting algorithm revision

---
+++ sorting_algos/selection_sort.py
@@ -0,0 +1,9 @@
+# iterate through array, swapping each element as far back as necessary
+
+from random import randint
+
+
+test_list = [randint(0, 10) for i in range(10)]
+
+def selection_sort(l):
+    pass
