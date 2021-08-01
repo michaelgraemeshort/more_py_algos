4eb322dc8ed9e4fd31bfeede57660a9a245eedef
michaelgraemeshort <michaelgraemeshort@gmail.com>
2021-08-01 22:27:29 +0100

    floyd warshall

--- graphs/more_testing.py
+++ graphs/more_testing.py
@@ -58,22 +58,76 @@ class Graph:
                         updating = True
         return "negative cycle present" if updating else shortest_path_lengths
 
+    def adjacency_matrix(self):
+        # returns a 2D list representation of self
+        # IF graph nodes are "a", "b", etc
+        matrix = [[float("inf") for i in range(len(self.nodes))] for j in range(len(self.nodes))]
+        for i in range(len(self.nodes)):
+            matrix[i][i] = 0
+        for i, j in self.edge_lengths.keys():
+            # convert to integers
+            matrix[ord(i) - 97][ord(j) - 97] = self.edge_lengths[(i, j)]
+        # print matrix
+        # for row in matrix:
+        #     print(row)
+        return matrix
+
+    def floyd_warshall(self):
+        # iterates through 2D array of path lengths
+        # and on each iteration compares existing path length with path length via another node
+        # iterates through all the other nodes
+        # for example, might compare the direct path from A to C with the path A -> B -> C and later A -> D -> C
+        # but: if A -> D -> C is shortest, and this is discovered late in the process...
+        # then any other paths that rely on A -> C that have already been determined will not be the shortest
+        # clearly this is not a problem, but why
+        # if the shortest path from A to B is via C, and later discover that A to C can be shortened via D, how to update A to B?
+        # in that case the shortest path won't be via C, it will be via C AND D
+        # so not a problem
+        shortest_path_lengths = self.adjacency_matrix()
+        number_of_nodes = len(self.nodes)
+        for k in range(number_of_nodes):
+            for i in range(number_of_nodes):
+                for j in range(number_of_nodes):
+                    shortest_path_lengths[i][j] = min(shortest_path_lengths[i][j],
+                                                      shortest_path_lengths[i][k] + shortest_path_lengths[k][j])
+        return shortest_path_lengths
 
 
 graph = Graph()
 
-for i in "abcdefg":
+# for i in "abcdefg":
+#     graph.add_node(i)
+
+# graph.add_edge("a", "b", 2)
+# graph.add_edge("a", "c", 5)
+# graph.add_edge("b", "c", 6)
+# graph.add_edge("b", "d", 1)
+# graph.add_edge("b", "e", 3)
+# graph.add_edge("c", "f", 8)
+# graph.add_edge("d", "e", 4)
+# graph.add_edge("e", "g", 9)
+# graph.add_edge("f", "g", 7)
+
+# print(graph.dijkstra("a"))
+# print(graph.bellman_ford("a"))
+
+# graph.adjacency_matrix()
+
+for i in "abcd":
     graph.add_node(i)
 
-graph.add_edge("a", "b", 2)
-graph.add_edge("a", "c", 5)
-graph.add_edge("b", "c", 6)
-graph.add_edge("b", "d", 1)
-graph.add_edge("b", "e", 3)
-graph.add_edge("c", "f", 8)
-graph.add_edge("d", "e", 4)
-graph.add_edge("e", "g", 9)
-graph.add_edge("f", "g", 7)
+graph.add_edge("a", "b", 6)
+graph.add_edge("b", "a", 6)
+graph.add_edge("a", "c", 3)
+graph.add_edge("c", "a", 3)
+graph.add_edge("a", "d", 1)
+graph.add_edge("d", "a", 1)
+graph.add_edge("b", "c", 2)
+graph.add_edge("c", "b", 2)
+graph.add_edge("c", "d", 1)
+graph.add_edge("d", "c", 1)
 
-print(graph.dijkstra("a"))
-print(graph.bellman_ford("a"))
+
+argh = graph.floyd_warshall()
+for row in argh:
+    print(row)
