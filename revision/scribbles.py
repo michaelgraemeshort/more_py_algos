87f74e3c51fe47738a85e882868213c5549176ec
michaelgraemeshort <michaelgraemeshort@gmail.com>
2021-12-30 13:01:40 +0000

    singly linked list madness

---
+++ revision/scribbles.py
@@ -0,0 +1,21 @@
+start = 0
+stop = 10
+step = 3
+
+# n = start
+# while n < stop:
+#     for i in range(step):
+#         if i == step - 1:
+#             print(n)
+#     n += 1
+
+# wtf 
+
+
+# print(start)
+# while start < stop:
+#     for i in range(step):
+#         start += 1
+#         if i == step - 1:
+#             print(start)
+
--- revision/singly_linked_list.py
+++ revision/singly_linked_list.py
@@ -1,10 +1,32 @@
-# singly linked list with common operations, modelled on built-in list type
+# singly linked list class with common operations, loosely modelled on built-in list type
+# Todo: tests, error handling
+
 
 class Node:
-    """Node for singly linked list."""
+    """Node for LinkedList class."""
     def __init__(self, value):
         self.value = value
-        self.next = None 
+        self.next = None
+
+    # implementing the below to enable max and min functions
+
+    def __lt__(self, other):
+        return True if self.value < other.value else False
+
+    # def __le__(self, other):
+    #     return True if self.value <= other.value else False 
+
+    # def __eq__(self, other):
+    #     return True if self.value == other.value else False  
+
+    # def __ne__(self, other):
+    #     return True if self.value != other.value else False 
+
+    def __gt__(self, other):
+        return True if self.value > other.value else False 
+
+    # def __ge__(self, other):
+    #     return True if self.value >= other.value else False
 
     def __str__(self):
         return str(self.value)
@@ -43,18 +65,34 @@ class LinkedList:
         return False
 
     def __getitem__(self, index):
-        """Return self[index]."""
-        if not self.head or index >= len(self):
-            raise IndexError("index out of range")
-        if index < 0:
-            raise IndexError("cannot index LinkedList by negative number")
-        node = self.head
-        for i in range(index):
-            node = node.next 
-        return node.value
+        """Return self[index]. Negative indices NOT supported."""
+        if isinstance(index, int):
+            if not self.head or index >= len(self):
+                raise IndexError("index out of range")
+            if index < 0:
+                raise IndexError("cannot index LinkedList by negative number")
+            node = self.head
+            for i in range(index):
+                node = node.next 
+            return node.value
+        if isinstance(index, slice):
+            output = []
+            start = 0 if not index.start else index.start
+            stop = len(self) if not index.stop or index.stop >= len(self) else index.stop   # nasty
+            step = 1 if not index.step else index.step
+            node = self.head 
+            for i in range(start):
+                node = node.next 
+            output.append(node.value)
+            for i in range((stop - start - 1) // step):
+                for j in range(step):
+                    node = node.next 
+                output.append(node.value)
+            return output
+        raise TypeError(f"List indices must be integers or slices")
 
     def __delitem__(self, index):
-        """Delete self[item]."""
+        """Delete self[item]. Negative indices NOT supported."""
         if not self.head or index >= len(self):
             raise IndexError("index out of range")
         if index < 0:
@@ -71,6 +109,41 @@ class LinkedList:
             previous_node = node
             node = node.next
         previous_node.next = node.next
+
+    def __add__(self, other):
+        """Returns self + other."""
+        # to behave like built-in list, should not modify self, but rather return concatenated list
+        result = LinkedList()
+        result.extend(self)
+        result.extend(other)
+        return result
+        # self.tail.next = other.head 
+        # self.tail = other.tail
+        # return self
+
+    def __iadd__(self, other):
+        """Return self += other."""
+        return self.__add__(other)
+
+    def __mul__(self, value):
+        """Return self * value."""
+        result = LinkedList()
+        for i in range(value):
+            result.extend(self)
+        return result
+        # if value == 0:
+        #     return self.clear()
+        # if value == 1:
+        #     return self
+        # copy_1 = self.copy()
+        # for i in range(value - 1):
+        #     copy_2 = copy_1.copy()
+        #     self += copy_2
+        # return self
+
+    def __imul__(self, value):
+        """Return self *= value."""
+        return self.__mul__(value)
 
     def __str__(self):
         output = []
@@ -102,8 +175,8 @@ class LinkedList:
             self.head = new_node
         self.length += 1
 
-    def pop(self):
-        """Remove and return last item."""
+    def pop(self, index=None):
+        """Remove and return item at index (default last)."""
         if not self.head:
             raise IndexError("pop from empty LinkedList")
         if len(self) == 1:
@@ -112,14 +185,22 @@ class LinkedList:
             self.tail = None 
             return node.value 
         previous_node = self.head
-        node = self.head.next 
-        while node.next:
-            previous_node = node
-            node = node.next 
-        self.tail = previous_node 
-        self.tail.next = None
-        self.length -= 1
-        return node.value
+        node = self.head.next
+        if index == 0:
+            return self.popleft()
+        if index == len(self) - 1 or not index: 
+            while node.next:
+                previous_node = node
+                node = node.next 
+            self.tail = previous_node 
+            self.tail.next = None
+            self.length -= 1
+            return node.value
+        for i in range(index - 1):
+            previous_node = node 
+            node = node.next
+        previous_node.next = node.next 
+        return node.value      
             
     def popleft(self):
         """Remove and return first item."""
@@ -168,7 +249,6 @@ class LinkedList:
         if len(self) == 1:
             self.pop()
             return 
-        # first node 
         if self.head.value == value:
             self.head = self.head.next 
             self.length -= 1
@@ -211,8 +291,9 @@ class LinkedList:
     def copy(self):
         """"Return a copy of the LinkedList."""
         copy_ = LinkedList()
-        for node in self:
-            copy_.append(node.value)
+        copy_.extend(self)
+        # for node in self:
+        #     copy_.append(node.value)
         return copy_
 
     def count(self, value):
@@ -225,21 +306,29 @@ class LinkedList:
 
     def extend(self, iterable):
         """Extend LinkedList by appending elements from the iterable."""
-        for i in iterable:
-            self.append(i)
+        if isinstance(iterable, LinkedList):
+            for i in iterable:
+                self.append(i.value)
+        else:
+            for i in iterable:
+                self.append(i)
+
+    def sum(self, start=0):
+        """Return the sum of the LinkedList. Optional start value (default 0)."""
+        output = 0
+        node = self.head 
+        for i in range(start):
+            node = node.next
+        while node:
+            output += node.value
+            node = node.next
+        return output 
 
     def sort(self):
         # TODO: revise Merge Sort, apply to LinkedList
         pass
     
-    # modify __getitem__ to handle slices
-    # modify pop() to handle arbitrary indices, like list.pop()
-    # error handling?
-
-
-    
-
-
-ll = LinkedList([1, 2, 3])
-ll.extend([4, 5, 6])
-print(ll)
+spam = LinkedList(range(5)) 
+spam *= 2
+print(spam)
+print(spam.sum(5))
